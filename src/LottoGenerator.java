d8aa0d0a042384a0ad3c47686769dff64af0ec40
jswdir <jswdir@gmail.com>
2024-06-09 23:08:02 +0900

    LottoGenerator.java

--- src/LottoGenerator.java
+++ src/LottoGenerator.java
@@ -1,17 +1,38 @@
-import java.util.HashSet;
-import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.Set;
+import java.util.TreeSet;
 
-public class LottoGenerator {
+class LottoGenerator {
+    private Set<Integer> generatedNumbers;
+
     public Set<Integer> generateNumbers() {
-        Set<Integer> numbers = new HashSet<>();
-        Random random = new Random();
-
-        while (numbers.size() < 6) {
-            int number = random.nextInt(45) + 1;
-            numbers.add(number);
+        generatedNumbers = new TreeSet<>();
+        while (generatedNumbers.size() < 6) {
+            int number = (int) (Math.random() * 45) + 1;
+            generatedNumbers.add(number);
         }
+        return generatedNumbers;
+    }
 
-        return numbers;
+    public Set<Integer> getGeneratedNumbers() {
+        return generatedNumbers;
+    }
+}
+
+class BackgroundPanel extends JPanel {
+    private BufferedImage image;
+
+    public BackgroundPanel(BufferedImage image) {
+        this.image = image;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (image != null) {
+            g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        }
     }
 }
