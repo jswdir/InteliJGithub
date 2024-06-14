4f983e85e2914e79a7e2f7a9f25ab51e66d56cf2
jswdir <jswdir@gmail.com>
2024-06-14 09:24:02 +0900

    LottoUI.java

--- src/LottoUI.java
+++ src/LottoUI.java
@@ -58,7 +58,7 @@ public class LottoUI extends JFrame {
         hyperlinkLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                openWebpage("https://docs.google.com/presentation/d/12DkI5TEYtfy4FcSdKBAUA-ytbvhyWsXf/edit#slide=id.p1");
+                openWebpage("https://docs.google.com/presentation/d/1ngvO83dD0z0aCZvKeJnNjhUDcim9alt3/edit#slide=id.p1");
             }
         });
         JPanel linkPanel = new JPanel();
